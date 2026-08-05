2446556f668e00017e66d2705f71f8343575791a
Robert Untila <tcrlabs548@gmail.com>
2026-08-05 23:52:47 +0300

    added first iteration of cses-tool integration, no docs yet

---
+++ src/Extensions/CSESTool.hpp
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2019-2021 Ashar Khan <ashar786khan@gmail.com>
+ *
+ * This file is part of CP Editor.
+ *
+ * CP Editor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * I will not be responsible if CP Editor behaves in unexpected way and
+ * causes your ratings to go down and or lose any important contest.
+ *
+ * Believe Software is "Software" and it isn't immune to bugs.
+ *
+ */
+
+#ifndef CSESTOOL_HPP
+#define CSESTOOL_HPP
+
+#include <QNetworkAccessManager>
+#include <QObject>
+#include <QString>
+#include <QJsonObject>
+#include <QTimer>
+#include <QPair>
+#include <QList>
+#include <QByteArray>
+
+namespace Extensions
+{
+
+class CSESTool : public QObject
+{
+    Q_OBJECT
+
+  public:
+    explicit CSESTool(QObject *parent = nullptr);
+
+    void login();
+    void checkLoginStatus();
+    void logout();
+    bool isLoggedIn() const;
+    QString savedToken() const;
+    QString savedScope() const;
+    void setScope(const QString &scope);
+    void clearToken(); // drop the stored token (e.g. when it's invalid); isLoggedIn() then returns false
+
+    void submitFile(const QString &scope,
+                    const QString &filePath,
+                    const QString &langName,
+                    const QString &langOption,
+                    const QString &taskId = {});
+
+    void fetchSubmission(const QString &scope, qint64 submissionId, bool longPoll = true);
+    void fetchSubmissionList(const QString &scope, const QString &taskId);
+    void fetchSamples(const QString &scope, const QString &taskId);
+    void fetchStatement(const QString &scope, const QString &taskId);
+    void fetchCourses();
+    void fetchCourseContent(const QString &scope);
+    void fetchTemplate(const QString &scope,
+                       const QString &taskId = {},
+                       const QString &langName = {},
+                       const QString &filename = {});
+
+    static bool parseCsesUrl(const QString &url, QString &scope, QString &taskId);
+
+  signals:
+    void loginFlowStarted(const QString &authUrl);
+    void loginSucceeded(const QString &username);
+    void loginPending();
+    void loginFailed(const QString &reason);
+    void logoutSucceeded();
+
+    void submissionCreated(qint64 submissionId);
+    void submissionUpdated(const QJsonObject &info);
+    void submissionFinished(const QJsonObject &info);
+    void submitError(const QString &errorCode, const QString &message);
+
+    void samplesReady(const QList<QPair<QByteArray, QByteArray>> &cases);
+    void statementReady(const QJsonObject &statement);
+    void coursesReady(const QJsonArray &courses);
+    void courseContentReady(const QJsonObject &content);
+    void templateReady(const QByteArray &source, const QString &filename);
+    void submissionListReady(const QJsonArray &submissions);
+
+    void networkError(const QString &message);
+
+  private:
+    QNetworkAccessManager *m_nam;
+    QString m_token;
+    QString m_scope;
+    bool m_loginFlowActive = false; // true between POST /login and confirmed browser login
+
+    void persistToken(const QString &token);
+
+    QString apiUrl(const QString &path) const;
+    QString scopedUrl(const QString &scope, const QString &endpoint) const;
+
+    QNetworkRequest authRequest(const QUrl &url) const;
+    QNetworkRequest anonRequest(const QUrl &url) const;
+
+    bool handleError(QNetworkReply *reply);
+    QString parseErrorCode(const QByteArray &body) const;
+    QString parseErrorMessage(const QByteArray &body) const;
+
+    void continuePollIfPending(const QJsonObject &info, const QString &scope, qint64 submissionId);
+};
+
+} // namespace Extensions
+
+#endif // CSESTOOL_HPP
